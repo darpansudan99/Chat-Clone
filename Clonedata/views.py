9e7bd5083deff5ca0f2a673206761f79cf010e28
Darpan Sudan <darpansudan99@gmail.com>
2023-06-25 18:36:29 +0530

    login and register page and some modification in navbar and home page

--- Clonedata/views.py
+++ Clonedata/views.py
@@ -6,3 +6,9 @@ def index(request):
 
 def about(request):
     return render(request, 'Clonedata/about.html')
+
+def register(request):
+    return render(request, 'Clonedata/register.html')
+
+def login(request):
+    return render(request, 'Clonedata/login.html')
